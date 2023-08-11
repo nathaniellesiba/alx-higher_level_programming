9a5140ed3289969c98511299344c1fb9d484424e
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 16:16:29 -0700

    2-args chmod

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,18 +2,20 @@
 
 import sys
 
-def print_arguments():
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
 
-if num_arguments == 0:
-print("Number of argument(s): 0.")
-print(".")
-else:
-print("Number of argument(s):", num_arguments)
-print("Arguments:")
-for i, arg in enumerate(arguments):
-print(i+1, ":", arg)
+def print_arguments():
+    arguments = sys.argv[1:]
+    num_arguments = len(arguments)
+
+    if num_arguments == 0:
+        print("Number of argument(s): 0.")
+        print(".")
+    else:
+        print("Number of argument(s):", num_arguments)
+        print("Arguments:")
+        for i, arg in enumerate(arguments):
+            print(i+1, ":", arg)
+
 
 if __name__ == "__main__":
-print_arguments()
+    print_arguments()
