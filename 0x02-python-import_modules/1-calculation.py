468e21dc623effacc88d2b3b4366db333a08c453
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 16:05:28 -0700

    cal chmod

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-""" display sum, subtraction, multiple,
-and division of 10 and 5."""
+    """ display sum, subtraction, multiple,
+    and division of 10 and 5."""
 
-from calculator_1 import add, sub, mul, div
+    from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+    a = 10
+    b = 5
 
-result1 = add(a, b)
-result2 = sub(a, b)
-result3 = mul(a, b)
-result4 = div(a, b)
+    result1 = add(a, b)
+    result2 = sub(a, b)
+    result3 = mul(a, b)
+    result4 = div(a, b)
 
-print("{} + {} = {}".format(a, b, result1))
-print("{} - {} = {}".format(a, b, result2))
-print("{} * {} = {}".format(a, b, result3))
-print("{} / {} = {}".format(a, b, result4))
+    print("{} + {} = {}".format(a, b, result1))
+    print("{} - {} = {}".format(a, b, result2))
+    print("{} * {} = {}".format(a, b, result3))
+    print("{} / {} = {}".format(a, b, result4))
