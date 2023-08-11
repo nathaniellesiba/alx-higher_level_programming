160e566579564bb010636fed360e7d4438496859
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-11 16:25:31 +0200

    102 diff

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 from magic_calculation_102 import add, sub
 
 def magic_calculation(a, b):
@@ -8,4 +9,4 @@ for i in range(4, 6):
 c = add(c, 1)
 return c
 else:
-return sub(a, b)
+return len(a) + len(b)
