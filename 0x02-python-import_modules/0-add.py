f6938b064f7ac34b8c2115a854fdf953bb7c96d4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-11 13:47:43 +0200

    first attempt

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def add(a, b):
+/**
+*a(int): the first number
+*b(int): the second number
+*Returns: the sum of two numbers
+*int: the values
+*/
+
+
+return a + b
+
+from add_0 import add
+a = 1
+b = 2
+
+result = add(a, b)
+
+print("{} + {} = {}".format(a, b, result))
