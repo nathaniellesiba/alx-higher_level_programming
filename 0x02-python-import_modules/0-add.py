d56ae01f9f9adc7e959689af8707e25a9f8f5208
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-11 15:13:10 +0200

    try 1

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 
-def add(a, b):
-/**
-*a(int): the first number
-*b(int): the second number
-*Returns: the sum of two numbers
-*int: the values
-*/
+if __name__ == '__main__':
+""" a(int): the first number
+b(int): the second number
+Returns: the sum of two numbers
+int: the values"""
 
 from add_0 import add
 a = 1
