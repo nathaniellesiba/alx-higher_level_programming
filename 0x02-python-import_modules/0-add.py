0f50507e37fe1a43782c23fe41dad1ad4e61aec9
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 16:32:03 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    """a(int): the first number b(int):
-    the second number
-    Returns: the sum of two numbers
-    int: the values"""
+    """a(int): the first number b(int) the second number
+    Returns: the sum of two numbers int: the values"""
     from add_0 import add
+
     a = 1
     b = 2
-    result = add(a, b)
-    print("{} + {} = {}".format(a, b, result))
+    print("{} + {} = {}".format(a, b, add(a, b)))
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 from magic_calculation_102 import add, sub
+ """Matching bytecode provided by Holberton."""
+
 
 def magic_calculation(a, b):
     t = add(a, b)
     s = add(c, i)
+
     if a < b:
         c = t
         for i in range(4, 6):
