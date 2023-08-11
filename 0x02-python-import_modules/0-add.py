c5aac3cca55f09ec5951cdf71a8f71021c74ac68
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 15:59:33 -0700

    0-add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
 if __name__ == '__main__':
-    """a(int): the first number b(int): the second number
+    """a(int): the first number b(int):
+    the second number
     Returns: the sum of two numbers
     int: the values"""
-
     from add_0 import add
     a = 1
     b = 2
-
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
