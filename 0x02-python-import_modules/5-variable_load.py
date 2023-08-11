ffeef006a177013c814a1e072844ab2dab703842
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 16:06:23 -0700

    5-variable_load

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
     """This function imports the variable 'a'from the
     file 'variable_load_5.py' and prints its value.
-    It follows the guidelines of not using '*' for 
+    It follows the guidelines of not using '*' for
     importing or '__import__' The code is not executed when imported."""
 
 from variable_load_5 import a
