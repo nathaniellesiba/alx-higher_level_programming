fb41f919f65d2c3394be26e263402c7f35dbfd92
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 16:18:28 -0700

    4-hidden_discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
 
-"""
-This function prints all the names defined by the compiled module hidden_4.pyc.
-It prints one name per line, in alphabetical order, excluding names that start with '__'.
-"""
+if __name__ == "__main__":
+    """This function prints all the names defined by the
+    compiled module hidden_4.pyc. It prints one name per line,
+    in alphabetical order, excluding names that start with '__'."""
 
-import hidden_4
+    import hidden_4
 
-names = dir(hidden_4)
-sorted_names = sorted(names)
-for name in sorted_names:
-if not name.startswith('__'):
-print(name)
-
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
