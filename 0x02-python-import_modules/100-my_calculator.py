4a13831cd9d372aeb0a8574e500f08fbd2825441
nathaniel <nathaniellesiba@yahoo.com>
2023-08-11 17:49:53 -0700

    100 chmod

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,20 @@
 #!/usr/bin/python3
 
-if__name__ == "__main__":
+if __name__ == "__main__":
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul, div
+    import sys
+    t = sys.exit(1)
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        t
+    operator = int(sys.argv[2])
 
-import sys
-from calculator_1 import add, subtract, multiply, divide
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if operator not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        t
 
-if len(sys.argv) != 4:
-print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-sys.exit(1)
-
-a = int(sys.argv[1])
-operator = sys.argv[2]
-b = int(sys.argv[3])
-
-if operator == '+':
-result = add(a, b)
-elif operator == '-':
-result = subtract(a, b)
-elif operator == '*':
-result = multiply(a, b)
-elif operator == '/':
-result = divide(a, b)
-else:
-print("Unknown operator. Available operators: +, -, * and /")
-sys.exit(1)
-
-print(f"{a} {operator} {b} = {result}")
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, operator, b, ops[operator](a, b)))
