af93c881d069035f226dde6f9b39cba382b70e0b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-14 22:57:12 +0200

    rectufued tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a ([0,0])
-    if tuple_b ([0,0])
-    
-"""Add corresponding elements frm each tuple"""
+    if tuple_a == ():
+        tuple_a = (0, 0)
+    if tuple_b == ():
+        tuple_b = (0, 0)
+
     sum_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    
+
     return sum_tuple
