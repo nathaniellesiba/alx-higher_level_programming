35ac40e5d3114cf85f77c47399e5c8107592fe34
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-14 21:21:41 +0200

    retrieval1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 """retrieves element from list like in C"""
 def element_at(my_list, idx):
- if idx < 0 or idx >= len(my_list):
-        return(None)
-    else:
-        return(my_list[idx])
+    return(my_list[idx] if idx >= 0 and idx < len(my_list) else None)
