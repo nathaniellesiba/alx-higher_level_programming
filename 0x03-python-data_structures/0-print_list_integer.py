282c736de317b41859865fcda69096433afb6872
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-14 20:48:55 +0200

    second try

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,5 @@
 
 def print_list_integer(my_list=[]):
     """prints all integers of a list"""
-    for i in _list:
-        if isinstance(i, int):
-            print("{:d}".format(i))
+    for num in _list:
+        print("{}".format(num))
