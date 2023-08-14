036d54154fffb30d180da0ea119f0914116e6a99
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-14 22:09:06 +0200

    matrix2

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,9 +2,9 @@
 
 """prints a matrix of integers"""
 """assume list only contains integers"""
+
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-"""The end=" " argument in the print() function ensures that each number is printed with a space separator. After printing all the numbers in a row, it moves to the next line using another print() statement"""
-            print("{:d}".format(num), end=" ")
+    for i in matrix:
+        for num in i:
+            print(''.join("{:d}".format(num)), end=" ")
         print()
