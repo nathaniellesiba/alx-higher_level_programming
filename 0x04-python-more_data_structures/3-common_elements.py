9f972e97ed7a899b50f518bf9b4eb8adb2b94be2
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-17 00:33:17 +0200

    common elements

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+"""The code defines a function called common_elements that takes two sets, set_1 and set_2, as input. It uses the intersection method to find the common elements between the two sets and returns a new set containing those elements"""
+
+def common_elements(set_1, set_2):
+common = set_1.intersection(set_2)
+        return common
