aa56e5319630a3f9c762211b0836cea17211d9bc
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-17 01:09:33 +0200

    del

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""The fixed code uses the del keyword to remove the specified key from the a_dictionary. It then returns the modified dictionary.By using the del keyword, we can directly delete the key-value pair from the dictionary without the need for additional steps. This ensures that the code is concise and efficient."""
+
+def simple_delete(a_dictionary, key=""):
+    del a_dictionary[key]
+    return a_dictionary
