bf8a96609b68988b3872605f2d52c23f20309485
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-17 00:16:55 +0200

    task 1 try

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-"""function that computes square value of all int of a matrix."""
+"""function that computes square value of all int of a matrix using ** operator."""
 
 def square_matrix_simple(matrix=[]):
     return [[num**2 for num in row] for row in matrix]
