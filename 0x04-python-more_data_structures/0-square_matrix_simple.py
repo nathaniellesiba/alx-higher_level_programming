baf90cdb8c45997c0ad3e95dbc984d7c00a3a584
nathaniel <nathaniellesiba@yahoo.com>
2023-08-16 16:52:00 -0700

    fix error1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""function that computes square value of all int of a matrix using ** operator."""
+"""function that computes square val of all int of matrix using ** opertr."""
+
 
 def square_matrix_simple(matrix=[]):
     return [[num**2 for num in row] for row in matrix]
