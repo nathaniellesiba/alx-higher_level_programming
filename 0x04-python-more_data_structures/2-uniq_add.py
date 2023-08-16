083d088560208792fdf14d680ff45f3c0fac4787
nathaniel <nathaniellesiba@yahoo.com>
2023-08-16 16:56:14 -0700

    fix error 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
-"""takes a list as input, uses the set() function to remove duplicate elements, leaving only unique int. Then, the sum() function is used to calculate the sum of all the unique integers in the list. Finally, the sum is returned as the output"""
+"""takes a list as input, uses the set() function to remove"""
+"""duplicate elements, leaving only unique int. Then, the sum() function"""
+"""is used to calculate the sum of all the unique integers in the list"""
+""". Finally, the sum is returned as the output"""
+
 
 def uniq_add(my_list=[]):
     return sum(set(my_list))
