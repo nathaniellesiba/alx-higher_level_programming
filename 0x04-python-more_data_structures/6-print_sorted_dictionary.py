7de11ad13101a320b59e674517e7e08af3b9e49b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-17 00:56:48 +0200

    sort

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""This code snippet defines a function print_sorted_dictionary that takes a dictionary a_dictionary as input. It creates a list list_order containing the keys of the dictionary using the keys() method. The list is then sorted in ascending order using the sort() method. Finally, a loop iterates over the sorted list and prints each key-value pair in the format "key: value"."""
+
+def print_sorted_dictionary(a_dictionary):
+    list_order = list(a_dictionary.keys())
+    list_order.sort()
+    for key in list_order:
+        print("{}: {}".format(key, a_dictionary[key]))
