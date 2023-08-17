212e652ae0f3e63fb0f35729e2e09ba356f438cc
nathaniel <nathaniellesiba@yahoo.com>
2023-08-16 17:08:46 -0700

    rror7

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
-"""This function takes 3 args[a_dictionary: The dictionary that needs to be updated. key: The key that needs to be updated or added. value: The value that needs to be assigned to the key. The function first checks if the key already exists in the dictionary. If it does, the value associated with the key is replaced with the new value. If the key doesn't exist, a new key-value pair is added to the dictionary."""
+"""This function takes 3 args[a_dictionary: The dictionary that needs to be"""
+"""updated. key: The key that needs to be updated or added."""
+"""value: The value that needs to be assigned to the key."""
+"""The function first checks if the key already exists in the dictionary."""
+"""If it does, the value associated with the key is replaced with the"""
+"""new value. If the key doesn't exist, a new key-value"""
+"""pair is added to the dictionary."""
+
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
