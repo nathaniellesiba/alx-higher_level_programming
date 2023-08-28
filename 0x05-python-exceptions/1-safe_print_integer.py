ec0c0f49b470195409fa7881bc74db0b7f912a41
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-29 00:00:36 +0200

    removed variable

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-value = 0
-
 try:
-
-print("{:d}".format(0))
+print("{:d}".format(value))
 return (True)
-
 except(TypeError, ValueError):
-
 return (False)
