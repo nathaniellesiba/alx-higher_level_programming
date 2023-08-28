2a75586e7612d4b7f71bc51e7008c884708bdc6b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-28 23:33:55 +0200

    except

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-try:
+
 count = 0
 for i in range(x):
+try:
 print("{:d}".format(my_list[i]), end=" ")
 count += 1
-except (IndexError):
+except (ValueError, IndexError, TypeError):
 pass
 finally:
 print("")
