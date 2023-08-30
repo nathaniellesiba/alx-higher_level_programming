dbf8a13c26a1e160826285620b5caad57a87d4c8
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-30 23:10:05 +0200

    space

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return (True)
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return (False)
+    return (False)
