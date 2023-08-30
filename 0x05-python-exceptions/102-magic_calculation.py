bb3aa72d2f44c9c8fb33016b51c4184c2f7435a2
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-30 23:22:32 +0200

    test

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
-        result += (a ** b) / i
-        return (result)
+        try:
+            if i > a:
+                raise Exception('Far out')
+            result += (a ** b) / i
+        except Exception:
+            result = b + a
+
+    return (result)
