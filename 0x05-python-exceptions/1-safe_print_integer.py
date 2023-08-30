0d4ad5402337a91243633ffeee055c7707f3a16a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2023-08-30 22:39:05 +0200

    value add

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,9 +4,8 @@
 
 def safe_print_integer(value):
     """print value which is an int"""
-    value = 0
     try:
-        print("{:d}".format(0))
+        print("{:d}".format(value))
         return (True)
     except (TypeError, ValueError):
         return (False)
