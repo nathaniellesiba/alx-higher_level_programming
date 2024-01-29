3562211c6a2a59928dd5080b9316fe02ff18779a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-01-29 23:37:30 +0200

    pass

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,13 +1,4 @@
 #!/bin/bash/python3
 
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
-
-    def area(self):
-        return self.length * self.width
-
-    def perimeter(self):
-        return 2 * (self.length + self.width)
-
+    pass
