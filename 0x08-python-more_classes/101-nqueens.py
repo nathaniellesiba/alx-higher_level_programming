a344b8e4341e53e73245c2200b3d3053f7007d5a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-01-30 00:21:09 +0200

    les

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,43 +2,58 @@
 
 import sys
 
-def is_safe(board, row, col, N):
+def is_safe(board, row, col):
+    # Check if there is a queen in the same column
     for i in range(row):
-        if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
+        if board[i][col] == 'Q':
             return False
+    
+    # Check if there is a queen in the upper left diagonal
+    i = row - 1
+    j = col - 1
+    while i >= 0 and j >= 0:
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j -= 1
+    
+    # Check if there is a queen in the upper right diagonal
+    i = row - 1
+    j = col + 1
+    while i >= 0 and j < len(board):
+        if board[i][j] == 'Q':
+            return False
+        i -= 1
+        j += 1
+    
     return True
 
-def solve_nqueens_util(board, row, N, solutions):
-    if row == N:
-        solutions.append(list(board))
-    else:
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row] = col
-                solve_nqueens_util(board, row + 1, N, solutions)
-
-def solve_nqueens(N):
-    if not N.isdigit():
+def solve_nqueens(n):
+    if not isinstance(n, int):
         print("N must be a number")
         sys.exit(1)
-    N = int(N)
-    if N < 4:
+    
+    if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    board = [-1] * N
+    
+    board = [['.' for _ in range(n)] for _ in range(n)]
     solutions = []
-    solve_nqueens_util(board, 0, N, solutions)
-
+    
+    def backtrack(row):
+        if row == n:
+            solutions.append([''.join(row) for row in board])
+            return
+        
+        for col in range(n):
+            if is_safe(board, row, col):
+                board[row][col] = 'Q'
+                backtrack(row + 1)
+                board[row][col] = '.'
+    
+    backtrack(0)
+    
     for solution in solutions:
-        print(','.join(str(col) for col in solution))
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    N = sys.argv[1]
-    solve_nqueens(N)
+        print('\n'.join(solution))
+        print()
 
