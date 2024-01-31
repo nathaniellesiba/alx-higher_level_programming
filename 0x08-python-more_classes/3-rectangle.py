44f1e224997844bd765ed17258d9793cb8ebfd59
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-01 01:35:22 +0200

    try3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -2,6 +2,8 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
+        self.__width = 0
+        self.__height = 0
         self.width = width
         self.height = height
 
@@ -41,4 +43,10 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width for _ in range(self.height)])
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
 
