8a86f6578ccc90058cbfd239df8de337afbd2569
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-03 01:25:20 +0200

    end

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,11 +1,29 @@
 #include <Python.h>
 
-void print_python_string(PyObject *p) {
-    if (PyUnicode_Check(p)) {
-        const char *str = PyUnicode_AsUTF8(p);
-        printf("Python String = %s\n", str);
-    } else {
-        printf("Error: Not a valid Python string\n");
+void print_python_string(PyObject *p)
+{
+    printf("[.] string object info\n");
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
     }
+
+    Py_ssize_t len;
+    const wchar_t *str = PyUnicode_AsWideCharString(p, &len);
+
+    if (str == NULL)
+    {
+        printf("  [ERROR] Failed to retrieve string\n");
+        return;
+    }
+
+    const char *type = PyUnicode_IS_COMPACT_ASCII(p) ? "compact ascii" : "compact unicode object";
+
+    printf("  type: %s\n", type);
+    printf("  length: %ld\n", len);
+    wprintf(L"  value: %ls\n", str);
+
+    PyMem_Free((void *)str);  // Free the memory allocated by PyUnicode_AsWideCharString
 }
 
