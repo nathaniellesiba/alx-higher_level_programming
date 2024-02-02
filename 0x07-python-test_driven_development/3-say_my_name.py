6e0ab8175c90b7323c44af43b7719e1086e56736
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-03 00:47:33 +0200

    cp7

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,10 +2,12 @@
 
 def say_my_name(first_name, last_name=""):
 
-if type(first_name) != str:
+if not isinstance(first_name, str):
 raise TypeError("first_name must be a string")
-
-if type(last_name) != str:
+if not isinstance(last_name, str):
 raise TypeError("last_name must be a string")
 
-print("My name is {:s} {:s}".format(first_name, last_name)) 
+if last_name:
+print(f"My name is {first_name} {last_name}")
+else:
+print(f"My name is {first_name}") 
