4d54fd9fc3938150d40854b11637630ea0a737f5
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-03 01:17:43 +0200

    100

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,31 +1,47 @@
 #!/usr/bin/python3
+def transpose(matrix):
+    """ Transpose a matrix """
+    # Transpose the matrix by using list comprehension
+    return [[row[i] for row in matrix] for i in range(len(matrix[0]))]
+
+
+def dot_product(row_a, row_b):
+    """ Multiply dot 2 matrix """
+    # Use the built-in sum function and list comprehension to calculate the dot product
+    return sum(a * b for a, b in zip(row_a, row_b))
+
+
 def matrix_mul(m_a, m_b):
+    """ Perform matrix multiplication """
+    # Validate the input matrices as per the given requirements
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if not m_a or any(not row for row in m_a):
+        raise ValueError("m_a can't be empty")
+    if not m_b or any(not row for row in m_b):
+        raise ValueError("m_b can't be empty")
+    if any(not all(isinstance(item, (int, float)) for item in row) for row in m_a):
+        raise TypeError("m_a should contain only integers or floats")
+    if any(not all(isinstance(item, (int, float)) for item in row) for row in m_b):
+        raise TypeError("m_b should contain only integers or floats")
+    if len(set(len(row) for row in m_a)) != 1:
+        raise TypeError("each row of m_a must be of the same size")
+    if len(set(len(row) for row in m_b)) != 1:
+        raise TypeError("each row of m_b must be of the same size")
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
 
-if not isinstance(m_a, list) or not all(isinstance(row, list) for row in m_a):
-raise TypeError("m_a must be a list of lists")
-if not m_a or any(not row for row in m_a):
-raise ValueError("m_a can't be empty")
-if not all(isinstance(num, (int, float)) for row in m_a for num in row):
-raise TypeError("m_a should contain only integers or floats")
-if len(set(len(row) for row in m_a)) != 1:
-raise TypeError("each row of m_a must be of the same size")
+    # Transpose matrix m_b
+    t_b = transpose(m_b)
 
-if not isinstance(m_b, list) or not all(isinstance(row, list) for row in m_b):
-raise TypeError("m_b must be a list of lists")
-if not m_b or any(not row for row in m_b):
-raise ValueError("m_b can't be empty")
-if not all(isinstance(num, (int, float)) for row in m_b for num in row):
-raise TypeError("m_b should contain only integers or floats")
-if len(set(len(row) for row in m_b)) != 1:
-raise TypeError("each row of m_b must be of the same size")
+    # Perform matrix multiplication using list comprehension
+    result = [[dot_product(row_a, row_b) for row_b in t_b] for row_a in m_a]
 
-if len(m_a[0]) != len(m_b):
-raise ValueError("m_a and m_b can't be multiplied")
+    return result
 
-result = [[0 for _ in range(len(m_b[0]))] for _ in range(len(m_a))]
-for i in range(len(m_a)):
-for j in range(len(m_b[0])):
-for k in range(len(m_b)):
-result[i][j] += m_a[i][k] * m_b[k][j]
-
-return result
