b114752c6175e9994fa7d37fc3ac7380a15847c4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-02 22:54:32 +0200

    test diff

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
-    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
-        raise TypeError("a must be an integer or b must be an integer")
 
     a = int(a)
     b = int(b)
+
+
+    if ((not isinstance(a, int) and not isinstance(a, float) 
+        raise TypeError("a must be an integer")
+
+    if ((not isinstance(b, int) and not isinstance(b, float)
+        raise TypeError("b must be an integer")
 
     return a + b
