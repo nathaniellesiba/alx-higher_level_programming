27f4180d974186486f5e31654ad1d51475e8fea0
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-03 01:20:18 +0200

    mumpy

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,4 +2,13 @@
 import numpy as np
 
 def lazy_matrix_mul(m_a, m_b):
-    return np.matmul(m_a, m_b)
+
+try:
+# Convert the input lists into numpy arrays and perform matrix multiplication
+result = np.dot(np.array(m_a), np.array(m_b))
+return result
+except ValueError:
+raise ValueError("m_a and m_b can't be multiplied")
+except Exception as e:
+raise type(e)(str(e))
+
