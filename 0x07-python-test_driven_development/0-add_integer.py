e511411373a862fe6c0a639b2ea7ea0f058fb844
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-03 00:10:43 +0200

    t6

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 
-"""
-add_integer:
-    check if Variable in int
-    Returns sum of Variable
-    """
-
 def add_integer(a, b=98):
 
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer or float")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer or float")
 
     return int(a) + int(b)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
