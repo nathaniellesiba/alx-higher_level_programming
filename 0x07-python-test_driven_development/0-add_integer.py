b526dc4a6edeb1824d5362c97942fe254e427cb3
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-02 23:31:11 +0200

    so conf

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python3
 
 def add_integer(a, b=98):
+    """
+    Adds 2 integers.
+
+    Args:
+        a (int or float): The first number to be added.
+        b (int or float): The second number to be added. Defaults to 98.
+
+    Returns:
+        int: The sum of a and b, cast to an integer.
+
+    Raises:
+        TypeError: If a or b are not integers or floats.
+
+    Examples:
+        >>> add_integer(1, 2)
+        3
+        >>> add_integer(100, -2)
+        98
+        >>> add_integer(2)
+        100
+        >>> add_integer(100.3, -2)
+        98
+        >>> add_integer(4, "School")
+        Traceback (most recent call last):
+            ...
+        TypeError: b must be an integer
+        >>> add_integer(None)
+        Traceback (most recent call last):
+            ...
+        TypeError: a must be an integer
+    """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer or float")
+        raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer or float")
+        raise TypeError("b must be an integer")
 
     return int(a) + int(b)
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/2-matrix_divided.txt")
