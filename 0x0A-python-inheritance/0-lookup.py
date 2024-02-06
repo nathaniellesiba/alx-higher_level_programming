1ced25bf3b951635d13800b65441ae1e89ebdea6
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-06 16:49:46 +0200

    testclass1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -2,14 +2,3 @@
 
 def lookup(obj):
     return dir(obj)
-
-class tsutsa:
-    def __init__(self):
-        self.x = 5
-        self.y = 10
-    def some_method(self):
-        pass
-
-obj = tsutsa()
-print(lookup(obj))
-
