d203345d2d8ad77a1e8bdbe5f0bb552b89c2e5e4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 01:31:40 +0200

    comments

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -2,10 +2,12 @@
 '''Module to lookup for the method'''
 
 def pascal_triangle(n):
+    '''given prototype'''
     if n <= 0:
         return []
 
     triangle = [[1]]
+    '''list with the first row of Pascal's triangle'''
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
