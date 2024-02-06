040b1867e8a28367db79b77c76408b03e051095c
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-02-06 15:48:42 -0800

    2-append comments

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 '''module to lookup for the method'''
+
 def append_write(filename="", text=""):
+    '''give prototype to use'''
+
     with open(filename, 'a', encoding='utf-8') as file:
         file.write(text)
     return len(text)
