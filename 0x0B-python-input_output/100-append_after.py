0fc09650f55e3f8f64792f66e49b8d156caf4460
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 01:34:31 +0200

    100 comments

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -7,7 +7,9 @@ def append_after(filename="", search_string="", new_string=""):
         lines = file.readlines()
         file.seek(0)
         '''to start writing from the beginning'''
+
         for line in lines:
+            '''storing in lines variable'''
             file.write(line)
             if search_string in line:
                 file.write(new_string + '\n')
