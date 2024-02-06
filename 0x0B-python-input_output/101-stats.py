e8e6c747b3fbe17224f644a210b3768ca67db161
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 01:13:29 +0200

    T8

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,49 +1,30 @@
 #!/usr/bin/python3
 '''Module to lookup for the method'''
 
-import signal
+import sys
 
-#Define a signal handler to handle keyboard interruption (CTRL + C)
-def signal_handler(sig, frame):
-    print_statistics()
-
-# Initialize variables to store statistics
-total_file_size = 0
-status_code_count = {
-    200: 0,
-    301: 0,
-    400: 0,
-    401: 0,
-    403: 0,
-    404: 0,
-    405: 0,
-    500: 0
-}
-
-# Function to print the statistics
-def print_statistics():
-    print(f"Total file size: {total_file_size}")
-    for status_code in sorted(status_code_count.keys()):
-        if status_code_count[status_code] > 0:
-            print(f"{status_code}: {status_code_count[status_code]}")
-
-# Register the signal handler for keyboard interruption
-signal.signal(signal.SIGINT, signal_handler)
-
-# Main loop to read and process input
-try:
+def compute_metrics():
+    total_size = 0
+    status_codes = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0}
     line_count = 0
-    while True:
-        input_line = input("Enter the input line: ")
-        parts = input_line.split()
-        file_size = int(parts[-1])
-        total_file_size += file_size
-        status_code = int(parts[-2])
-        if status_code in status_code_count:
-            status_code_count[status_code] += 1
-        line_count += 1
-        if line_count % 10 == 0:
-            print_statistics()
-except KeyboardInterrupt:
-    print_statistics()
 
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            ip, _, _, status_code, file_size = line.split(' ')[0], line.split(' ')[-2], line.split(' ')[-1]
+            total_size += int(file_size)
+            status_codes[status_code] += 1
+
+            if line_count % 10 == 0:
+                print(f"Total file size: {total_size}")
+                for code in sorted(status_codes.keys()):
+                    if status_codes[code] > 0:
+                        print(f"{code}: {status_codes[code]}")
+
+    except KeyboardInterrupt:
+        print(f"Total file size: {total_size}")
+        for code in sorted(status_codes.keys()):
+            if status_codes[code] > 0:
+                print(f"{code}: {status_codes[code]}")
+
+compute_metrics()
