ee20e9502a4137b7d677f36b41d1c62a8b534ed4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 00:07:48 +0200

    readT2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
+#!/usr/bin/python3
+'''method that calls the module'''
+
 def read_file(filename=""):
+    '''prototype for read_file and its file name'''
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
             print(line, end='')
