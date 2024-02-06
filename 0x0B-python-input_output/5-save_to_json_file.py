78ca42937d4f5dc4c6fb04ad82d7184cd046e0ea
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 01:02:23 +0200

    T4

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Module to lookup for the method'''
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    with open(filename, 'w') as file:
+        json.dumb(my_obj, file)
