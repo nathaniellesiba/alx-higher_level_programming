c8817a4b9b967799bcbbf71fd5cf724e9a3c60a4
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-07 00:47:13 +0200

    piss

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module to lookup for the method'''
+
+import json
+'''import of json scripts into python'''
+
+def from_json_string(my_str):
+    '''the required prototype'''
+    return json.dumps(my_str)
