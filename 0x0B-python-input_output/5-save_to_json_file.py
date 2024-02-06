b1d97e3ef94e26fd2af50a23ee61e95364e09d78
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-02-06 15:52:27 -0800

    5-comments

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,7 +3,11 @@
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     '''the prototype required'''
+
     with open(filename, 'w') as file:
+        '''function to write the object to a text file in JSON format'''
+
         json.dumb(my_obj, file)
