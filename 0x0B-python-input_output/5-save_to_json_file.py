8b56e42b9a90449c08a2fe1f9e10ef25d65fe267
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-08 00:20:30 +0200

    T6

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,11 +3,16 @@
 
 import json
 
-
 def save_to_json_file(my_obj, filename):
-    '''the prototype required'''
+    """
+    Save an object to a text file using a JSON representation.
 
+    Args:
+    my_obj: The object to be serialized.
+    filename: The name of the file to save the JSON representation to.
+
+    Returns:
+    None
+    """
     with open(filename, 'w') as file:
-        '''function to write the object to a text file in JSON format'''
-
-        json.dumb(my_obj, file)
+        json.dump(my_obj, file)
