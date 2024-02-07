1d31e5501e00944a6ad947c7e64c68d47b46e20a
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-08 01:13:41 +0200

    balance

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,6 +4,3 @@ class TestClass:
     __slots__ = ['attr1']
 
 obj = TestClass()
-add_attribute(obj, 'attr1', 10)  # This will add the attribute 'attr1' to the object with the value 10
-add_attribute(obj, 'attr2', 20)  # This will raise a TypeError with the message "can't add new attribute"
-
