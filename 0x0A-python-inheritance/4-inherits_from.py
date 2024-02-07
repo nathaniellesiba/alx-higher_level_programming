69d62f38d45b987a713dcb8766cec6fbb14c3dbb
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-08 00:59:34 +0200

    comm

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 '''Module to lookup for the method'''
 
 def inherits_from(obj, a_class):
-'''
+   """
     Returns True if the object is an instance of a class that inherited (directly or indirectly) from the specified class; otherwise False.
 
     Args:
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
 
     Returns:
     True if obj is an instance of a class that inherited (directly or indirectly) from a_class; otherwise False.
-'''
+"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
 
 class ParentClass:
@@ -20,13 +20,8 @@ class ParentClass:
 class ChildClass(ParentClass):
     pass
 
-'''
-the inherits_from function with different objects and classes, it will return True if the object is an instance of a class that inherited (directly or indirectly) from the specified class
-'''
-
 obj1 = ChildClass()
 obj2 = "Hello"
 print(inherits_from(obj1, ParentClass))  # This will return True
 print(inherits_from(obj1, object))  # This will return True
 print(inherits_from(obj2, int))  # This will return False
-
