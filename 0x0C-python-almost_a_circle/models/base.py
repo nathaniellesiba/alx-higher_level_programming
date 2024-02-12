9b0d99f81c67a43f712d00c57c2f01b48346f650
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-12 22:27:27 +0200

    adding comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 
+"""Import libraries for base model"""
+#import json
+#import csv
+#import turtle
+
+
 class Base:
+
+    """This is the base model, 
+    it represent all classes in the project
+    and doing so with provate class attribute
+    """
     __nb_objects = 0  # private class attribute
 
     def __init__(self, id=None):
+        """ this is initializing a new base
+        """
         if id is not None:
-            self.id = id  # assign the public instance attribute id with the argument value
+            self.id = id  
+            """ if id is not none, then
+            assign the public instance attribute id with the argument value
+            """
         else:
             Base.__nb_objects += 1  # increment __nb_objects
             self.id = Base.__nb_objects  # assign the new value to the public instance attribute id
