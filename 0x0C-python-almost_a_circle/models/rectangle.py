60cb6e65c6a5493b673d37a78d88cdb8fbf05792
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-12 23:23:40 +0200

    removed typeError

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-
-from models.base import Base  # Import the Base class
+"""models for rectangle"""
+from models.base import Base
 
 class Rectangle(Base):
     """this is the rectangle class"""
@@ -20,12 +20,8 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value <= 0:
-            raise ValueError("width must be > 0")
-        else:
-            self.__width = value
+        self.validate_integer("width", value, False)
+        self.__width = value
 
     @property
     def height(self):
@@ -34,12 +30,8 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        elif value <= 0:
-            raise ValueError("height must be > 0")
-        else:
-            self.__height = value
+        self.validate_integer("height", value, False)
+        self.__height = value
 
     @property
     def x(self):
@@ -48,12 +40,8 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        elif value < 0:
-            raise ValueError("x must be >= 0")
-        else:
-            self.__x = value
+        self.validate_integer("x", value, False)
+        self.__x = value
 
     @property
     def y(self):
@@ -62,12 +50,8 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        elif value < 0:
-            raise ValueError("y must be >= 0")
-        else:
-            self.__y = value
+        self.validate_integer("y", value, False)
+        self.__y = value
 
     def area(self):
         return self.__width * self.__height
