1f860f039959a6491d248ec1583d4c995c42a46d
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-02-17 03:16:56 +0200

    taks 3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,11 +42,63 @@ class Rectangle(Base):
     def y(self, value):
         self.__y = value
 
+
+
 class Rectangle:
-    def __init__(self, length, width):
-        self.length = length
-        self.width = width
+    def __init__(self, width, height, x, y):
+        self._width = width
+        self._height = height
+        self._x = x
+        self._y = y
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self._height = value
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, value):
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self._x = value
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, value):
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self._y = value
 
     def area(self):
-        return self.length * self.width
+        return self._width * self._height
 
