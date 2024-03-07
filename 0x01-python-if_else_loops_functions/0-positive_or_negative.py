4fda15c33a1133037d24946af27429c841fd5ee5
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-08 01:00:08 +0200

    last

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10, 10)
 print(number)
 if number > 0:
-print("positive")
+    print("is positive")
 elif number == 0:
-print("zero")
+    print("is zero")
 else:
-print("negative")
+    print("is negative")
