076f01c089b3a317760d10e88174e788bde79f12
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-13 00:20:39 +0200

    12,13,103

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 --Import in hbtn_0c_0 database this table dump by state
-SELECT state, MAX(temperature) AS max_temperature FROM weather_data GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS max_temp FROM temperatures GROUP BY state ORDER BY max_temp DESC;
