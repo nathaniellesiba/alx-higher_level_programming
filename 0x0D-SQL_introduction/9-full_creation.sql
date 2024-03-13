56be4db61cd1ffdaabb5e12453021ca048832856
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-14 01:45:48 +0200

    9 rect

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,2 +1,11 @@
 -- creates a table second_table in the db hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (id INT, name VARCHAR(256), score INT); INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+CREATE TABLE second_table (
+    id INT,
+    name VARCHAR(50),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES
+(1, 'John', 10),
+(2, 'Alex', 3),
+(3, 'Bob', 14),
+(4, 'George', 8);
