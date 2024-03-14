82119e412900d6314b6c2039079d56ffac758bdb
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 01:52:30 +0200

    T6

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa) on your MySQL server.
+CREATE DATABASE hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
