2f4d255cb39219a39828b90d66e1574515f021b3
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 01:14:57 +0200

    101 value

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Import in hbtn_0c_0 database this table dump
-SELECT 'city', AVG('value') as 'avg_temp'
-FROM 'temperatures'
-GROUP BY 'city'
-ORDER BY 'avg_temp' DESC;
+SELECT city, AVG(value) as avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
