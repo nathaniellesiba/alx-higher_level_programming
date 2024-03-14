97a20b94095cc4c457b0cc44aac4bdbc28614ff5
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 01:19:52 +0200

    temp by month

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,7 @@
 -- Import in hbtn_0c_0 database this table dump by month
-SELECT city, MAX(value) AS max_temperature FROM weather_data WHERE MONTH(date) IN (7, 8) GROUP BY city ORDER BY max_temperature DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures_by_month
+WHERE MONTH(date) IN (7, 8)
+GROUP BY city
+ORDER BY avg_temp
+DESC LIMIT 3;
