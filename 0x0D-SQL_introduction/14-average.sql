b3ff7ab839481d5179b2b6e12136d4b18f1cc130
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 00:37:11 +0200

    _score

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- computes score average of all records in table second_table
-SELECT AVG(score) AS average FROM hbtn_0c_0.second_table;
+SELECT AVG(score) AS average_score
+FROM hbtn_0c_0.second_table;
