830390437f7372e63275891044b613d6cbb76cb1
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 00:47:02 +0200

    tes

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- lists the number of records with the same score
-SELECT score, COUNT(*) AS record_count
-FROM hbtn_0c_0.second_table
+SELECT score, COUNT(*) AS number
+FROM second_table
 GROUP BY score;
