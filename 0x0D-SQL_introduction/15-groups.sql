8cd9cc135d8545f8d0efbde4411c56d5f3bd767d
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 00:44:30 +0200

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- lists the number of records with the same score
-SELECT score, COUNT(*) AS number FROM hbtn_0c_0.second_table GROUP BY score ORDER BY number DESC;
+SELECT score, COUNT(*) AS record_count
+FROM hbtn_0c_0.second_table
+GROUP BY score;
