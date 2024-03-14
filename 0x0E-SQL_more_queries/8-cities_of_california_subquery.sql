4ef39955378c59ad19c83d85b8540bcf6797fbce
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 01:59:54 +0200

    rm -rf state id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,6 @@
 -- lists all the cities of California that can be found in the database
-SELECT cities.id, cities.name, cities.state_id
+SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = states.id AND states.name = 'California'
+WHERE cities.state_id = states.id
+AND states.name = 'California'
 ORDER BY cities.id ASC;
