aa70eee15ba02d1041c3d909fecaaf5884e24346
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 00:02:28 +0200

    T5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- Prints full desc table first_table from db hbtn_0c_0
-SELECT COLUMN_NAME, DATA_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA 
-FROM information_schema.COLUMNS 
-WHERE TABLE_SCHEMA = 'hbtn_0c_0' AND TABLE_NAME = 'first_table';
+SELECT column_name, data_type, character_maximum_length
+FROM information_schema.columns
+WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
