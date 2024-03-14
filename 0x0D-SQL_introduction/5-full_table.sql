1d28198f5d1e79943081fd55db3f5375032907d6
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 00:08:33 +0200

    T501

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,2 @@
 -- Prints full desc table first_table from db hbtn_0c_0
-SELECT column_name, data_type, character_maximum_length
-FROM information_schema.columns
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+SHOW FULL COLUMNS FROM first_table FROM hbtn_0c_0;
