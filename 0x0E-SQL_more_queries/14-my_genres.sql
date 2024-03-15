ff38a0b929bd19210f5472eb343696ac1d9807f6
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 02:28:43 +0200

    T15

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,5 @@
 -- lists all Comedy shows in the database
-SELECT name
+SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
