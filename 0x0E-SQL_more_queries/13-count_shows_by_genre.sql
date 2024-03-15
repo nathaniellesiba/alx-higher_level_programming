c2062381bafc6251bea479d7b43ff652197b272b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-03-15 02:24:40 +0200

    rectT13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 --  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
-SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+SELECT genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM tv_show_genres
 GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
