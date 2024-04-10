cd34d44ec8c2e0007a4cbcebd61620d9fbda2417
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-11 00:00:45 +0200

    t10cp

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
-
-const factorial = (n) => {
-  if (isNaN(n)) {
-    console.log(1);
-  } else if (n === 0 || n === 1) {
-    console.log(1);
-  } else {
-    console.log(n * factorial(n - 1));
+function factorial (n) {
+  if (n < 0) {
+    return (-1);
   }
-};
+  if (n === 0 || isNaN(n)) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
 
-const [,,arg] = process.argv;
-factorial(parseInt(arg));
+console.log(factorial(Number(process.argv[2])));
