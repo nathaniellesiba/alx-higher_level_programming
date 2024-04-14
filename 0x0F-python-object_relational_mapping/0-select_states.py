3e251576861dd6b6340f8dc9b8169b9bdad1885d
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 13:23:52 -0700

    indention

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,8 @@ import MySQLdb
 
 if __name__ == "__main__":
         username = sys.argv[1]
-            password = sys.argv[2]
-                db_name = sys.argv[3]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
 
                     db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name, port=3306)
                         cursor = db.cursor()
