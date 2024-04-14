9fb71d4fb284ed3112b29cc2cd2424f70125aa03
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-14 02:12:17 +0200

    zzzT0Test3

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,22 +1,12 @@
 #!/usr/bin/python3
-"""importing all lists from db"""
+
 import MySQLdb
 import sys
 
-"""execution order"""
-if__name__=='__main'__:
-     db= MySQLdb.connect(host='localhost',port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c=db.cursor()
-
-     """executing the SQL query"""
-    c.execute("SELECT * FROM states ORDER BY id ASC")
-
-    """fetching all rows"""
-    rows = c.fetchall()
-
-    """print results"""
-    for row in rows:
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cur.fetchall():
         print(row)
-
-    """close db connection"""
     db.close()
