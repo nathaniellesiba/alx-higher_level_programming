453134b4ec2d572a61f1190f42b69e6286d59a9f
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 12:43:04 -0700

    T0Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """list of states in ascending order"""
-import MySQLdb
-import sys
 
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-            passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-    cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+        username = sys.argv[1]
+            password = sys.argv[2]
+                db_name = sys.argv[3]
+
+                    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=db_name, port=3306)
+                        cursor = db.cursor()
+                            cursor.execute("SELECT * FROM states ORDER BY id")
+                                data = cursor.fetchall()
+                                    for row in data:
+                                                print(row)
+                                                    cursor.close()
+                                                        db.close()
