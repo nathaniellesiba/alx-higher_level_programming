c121ac82722b5cd9bd0647240688010f6a824c1a
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 13:34:22 -0700

    T4 indent fix

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,16 +7,16 @@ import MySQLdb
 
 def list_cities(username, password, database):
         db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
-            cursor = db.cursor()
-                cursor.execute("SELECT * FROM cities ORDER BY id")
-                    data = cursor.fetchall()
-                        for row in data:
-                                    print(row)
-                                        cursor.close()
-                                            db.close()
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM cities ORDER BY id")
+        data = cursor.fetchall()
+        for row in data:
+            print(row)
+        cursor.close()
+        db.close()
 
-                                            if __name__ == "__main__":
-                                                    username = sys.argv[1]
-                                                        password = sys.argv[2]
-                                                            database = sys.argv[3]
-                                                                list_cities(username, password, database)
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    list_cities(username, password, database)
