2d437345de8ac5a4aba99fee7d6a82a85263617c
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 13:43:18 -0700

    t5 indent fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,40 +6,17 @@ that state, using the database hbtn_0e_4_usa"""
 import sys
 import MySQLdb
 
-def list_cities_by_state(username, password, database, state_name):
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-        # Create a cursor object using cursor() method
-        cursor = db.cursor()
-
-        # Prepare the SQL query with a parameter to list cities of the given state sorted by id in ascending order
-        query = "SELECT * FROM cities WHERE state_name = %s ORDER BY id ASC"
-
-        # Execute the SQL query with the state_name as a parameter
-        cursor.execute(query, (state_name,))
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error: {}".format(e))
-    finally:
-        # Close the database connection
-        if db:
-            db.close()
-
 if __name__ == "__main__":
-    # Get the arguments from the command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+        username = sys.argv[1]
+        password = sys.argv[2]
+        database = sys.argv[3]
+        state_name = sys.argv[4]
 
-    # Call the function with the provided arguments
-    list_cities_by_state(username, password, database, state_name)
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM cities WHERE state_id IN (SELECT id FROM states WHERE name = %s)", (state_name,))
+        data = cursor.fetchall()
+        for city in data:
+            print(city)
+        cursor.close()
+        db.close()
