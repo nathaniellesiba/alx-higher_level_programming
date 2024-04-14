76655174e52689ec6f86eaab8551cb8c9619a87b
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-14 20:31:22 +0200

    T2 wrtsnc

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,21 +4,43 @@ and displays all values in the states
 table of hbtn_0e_0_usa where name
 matches the arg"""
 
-import MySQLdb
 import sys
+import MySQLdb
+
+def get_state_data(username, password, dbname, state_name):
+    try:
+        # Connect to the MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=dbname)
+
+        # Create a cursor object using cursor() method
+        cursor = db.cursor()
+
+        # Prepare a SQL query with parameters to prevent SQL injection
+        query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
+
+        # Execute the SQL query with the state_name as a parameter
+        cursor.execute(query, (state_name,))
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Display the results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("Error: {}".format(e))
+    finally:
+        # Close the database connection
+        if db:
+            db.close()
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name))
-
-    for row in cur.fetchall():
-        print(row)
-
-    cur.close()
-    db.close()
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <dbname> <state_name>")
+    else:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        dbname = sys.argv[3]
+        state_name = sys.argv[4]
+        get_state_data(username, password, dbname, state_name)
