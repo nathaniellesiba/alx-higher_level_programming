06a62651361acd186835879b1a2ff9ee43da077c
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 14:04:42 -0700

    TableChanges

--- 0x0F-python-object_relational_mapping/6-model_state.sql
+++ 0x0F-python-object_relational_mapping/6-model_state.sql
@@ -1,6 +1,10 @@
 -- Create database hbtn_0e_6_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0e_6_usa;
 USE hbtn_0e_6_usa;
-SHOW CREATE TABLE states;
-Table   Create Table
-states  CREATE TABLE `states` (\n  `id` int(11) NOT NULL AUTO_INCREMENT,\n  `name` varchar(128) NOT NULL,\n  PRIMARY KEY (`id`)\n) ENGINE=InnoDB DEFAULT CHARSET=latin1
+
+-- Create the states table
+CREATE TABLE IF NOT EXISTS states (
+	    id INT NOT NULL AUTO_INCREMENT,
+	    name VARCHAR(255) NOT NULL,
+	    PRIMARY KEY (id)
+	);
