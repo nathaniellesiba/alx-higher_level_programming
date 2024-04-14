f81ebcc3bc204df3aa30c303398f765fa52242c4
nathaniellesiba <nathaniellesiba@yahoo.com>
2024-04-14 12:50:22 -0700

    rect

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
 """lists all cities from the
 database hbtn_0e_4_usa"""
-
 import sys
 import MySQLdb
 
 def list_cities(username, password, database):
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(host="localhost",
-                port=3306, user=username,
-                passwd=password, db=database)
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+            cursor = db.cursor()
+                cursor.execute("SELECT * FROM cities ORDER BY id")
+                    data = cursor.fetchall()
+                        for row in data:
+                                    print(row)
+                                        cursor.close()
+                                            db.close()
 
-        # Create cursor object using cursor() method
-        cursor = db.cursor()
-
-        # list all cities sorted in asc order
-        query = "SELECT * FROM cities ORDER BY id ASC"
-
-        # Execute the SQL query
-        cursor.execute(query)
-
-        # Fetch all the rows
-        rows = cursor.fetchall()
-
-        # Display the results
-        for row in rows:
-            print(row)
-
-    except MySQLdb.Error as e:
-        print("Error: {}".format(e))
-    finally:
-        # Close the database connection
-        if db:
-            db.close()
-
-if __name__ == "__main__":
-    # Get the arguments from the command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call the function with the provided arguments
-    list_cities(username, password, database)
+                                            if __name__ == "__main__":
+                                                    username = sys.argv[1]
+                                                        password = sys.argv[2]
+                                                            database = sys.argv[3]
+                                                                list_cities(username, password, database)
