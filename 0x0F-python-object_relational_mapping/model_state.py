542e3d051b06caa44a810a5d30ce5415da7112f9
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 00:24:00 +0200

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,15 +11,6 @@ Base = declarative_base()
 
 class State(Base):
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-
-# Ensure to import all classes that inherit from Base before calling Base.metadata.create_all(engine)
-# Example:
-# from your_module import State
-
-# Create an engine to connect to the MySQL server running on localhost at port 3306
-engine = create_engine('mysql://username:password@localhost:3306/database_name')
-
-# Create the tables
-Base.metadata.create_all(engine)
