d94152de8bfe9a545810d2ae59a87f722b590112
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 00:57:46 +0200

    T16 mod

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,11 +11,16 @@ from sqlalchemy.orm import relationship
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+                           pool_pre_ping=True)
+    
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
+    
     session = Session()
     for instance in session.query(State).order_by(State.id):
         for city_ins in instance.cities:
             print(city_ins.id, city_ins.name, sep=": ", end="")
             print(" -> " + instance.name)
+
+    session.close()
