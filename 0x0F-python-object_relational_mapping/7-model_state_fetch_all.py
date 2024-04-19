285e109a2a7b36e1dd2b83988e13e75dca94d412
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 00:39:39 +0200

    T7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,44 +4,25 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State  # Import the model_state module
+from model_state import Base, State
 
-def list_states(username, password, database):
-        try:
-            # Create an engine to connect to the MySQL server
-            engine = create_engine(f"mysql://{username}:{password}@localhost:3306/{database}")
-
-            # Bind the engine to the Base class
-            Base.metadata.bind = engine
-
-            # Create a session
-            Session = sessionmaker(bind=engine)
-            session = Session()
-
-            # Query to retrieve all State objects and sort by id in ascending order
-            states = session.query(State).order_by(State.id).all()
-
-            # Display the results
-            for state in states:
-                print(f"{state.id}: {state.name}")
-
-        except Exception as e:
-            print(f"Error: {e}")
-        finally:
-            # Close the session
-            if session is not None:
-                session.close()
 
 if __name__ == "__main__":
-    # Ensure the required command-line arguments are provided
-    if len(sys.argv) < 4:
-        print("Usage: ./script.py <username> <password> <database>")
-        sys.exit(1)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+            (sys.argv[1],sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    # Get the arguments from the command line
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
 
-    # Call the function with the provided arguments
-    list_states(username, password, database)
+    """ create a session """
+    session = Session()
+
+    """ extract all states """
+    states = session.query(State).order_by(State.id).all()
+
+    """ print all states """
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
