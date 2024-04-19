8c57d30adb09f02235a2e0c7ab7126a698372baa
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 00:26:07 +0200

    Tmodel

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -4,13 +4,23 @@ the class definition of a State and
 an instance Base = declarative_base()"""
 
 
-from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy import Integer
+
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+        State class inherits the Base class
+        Attributes:
+            id (int)
+            name (string)
+    """
     __tablename__ = 'states'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
