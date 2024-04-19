4831a53b5293c9d2ff42f29a66df7a6d7b291c2f
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-20 00:53:13 +0200

    T14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,26 +8,24 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
-            format(username, password, db_name),pool_pre_ping=True)
-    
-    Base.metadata.create_all(engine)
+            format(
+                           sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
     Session = sessionmaker(bind=engine)
-    
-    """creating a session"""
+    Base.metadata.create_all(engine)
+
+    """create a session"""
     session = Session()
 
-    """extracting all cities in a state"""
+    """extract all cities in a state"""
     cities = session.query(State, City) \
-            .order_by(City.id).all()
+                    .filter(State.id == City.state_id)
 
     """print all states"""
-    for city in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
 
+    for ci in cities:
+        print("{}: ({}) {}".format(ci.State.name, ci.City.id, ci.City.name))
 
     session.close()
