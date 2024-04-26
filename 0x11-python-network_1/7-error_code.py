ca966a5992175ed2bd86ef8cdf4a475d257e36b0
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-27 01:58:56 +0200

    test

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """takes URL Sends a request
 to a given URL and displays the response body.
-Usage: ./7-error_code.py <URL>
-  - Handles HTTP errors.
 """
 import sys
 import requests
@@ -11,8 +9,6 @@ import requests
 if __name__ == "__main__":
     url = sys.argv[1]
     site = requests.get(url)
-
-"""if error code is less than 400 give name, code"""
     if site.status_code >= 400:
         print("Error code: {}".format(site.status_code))
     else:
