d7033691ab1b133f66379b3e66b29d5395579f57
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-27 01:16:47 +0200

    T1 try

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Displays the X-Request-Id header variable found in the header of response
+"""Displays the X-Request-Id header
+variable found in the header of response
 """
 import sys
 import urllib.request
 
-
 if __name__ == "__main__":
     url = sys.argv[1]
-
 """fetching the url specified in argv"""
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response:
-        print(dict(response.headers).get("X-Request-Id"))
+    with urllib.request.urlopen(url) as response:
+        html = response.getheader("X-Request-Id")
+        print(html)
