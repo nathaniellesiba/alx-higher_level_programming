6cf462b6e11fc3591a5c86026acba46d4f418465
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-27 01:31:54 +0200

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,19 +4,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-        url = "https://api.github.com/repos/{}/{}/commits".format(
-        sys.argv[2], sys.argv[1])
-
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-
-"""for alternative code
-
     if len(sys.argv) < 3:
         sys.exit(1)
     else:
@@ -26,6 +13,6 @@ if __name__ == "__main__":
         my_json = html.json()
         for member_list in my_json[:10]:
             print("{}: {}".format(member_list.get('sha'),
-                                  member_list.get('commit').get('author').
+                                  member_list.get('commit').
+                                  get('author').
                                   get('name')))
-"""
