05509f9e6e0b6ee64a3ff6d56a05f15fca837446
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-27 01:06:16 +0200

    T0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""fetches  https://alx-intranet.hbtn.io/status"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+a = type(html)
+b = html.decode("utf-8")
+print("Body response:")
+print("\t- type: {}".format(a))
+print("\t- content: {}".format(html))
+print("\t- utf8 content: {}".format(b))
