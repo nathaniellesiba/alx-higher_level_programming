c49dd4a4d2b0a26d158bd55470216289706c76a6
Tsutsa <nathaniel.tsutsa@standardbank.co.za>
2024-04-27 02:49:23 +0200

    alt

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """fetches https://alx-intranet.hbtn.io/status
-"""
 
 import requests
 
@@ -9,4 +8,14 @@ if__name__=="__main__":
     a = type(html.text)
     print("Body response:")
     print("\t- type: {}".format(a))
-    print("\t- content: {}".format(html.text))
+    print("\t- content: {}".format(html.text))"""
+
+
+import requests
+
+url = 'https://alx-intranet.hbtn.io/status'
+response = requests.get(url)
+data = response.json()
+
+print("- type: {}".format(type(data)))
+print("- content: {}".format(data))
